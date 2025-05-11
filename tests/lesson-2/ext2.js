ce7ad8198f066f902791f3e9dfeabed482fb39be
PW-Phuong <phuong2111@gmail.com>
2025-05-11 17:46:22 +0700

    feat: add tests/lesson-2/ex3.js

--- tests/lesson-2/ext2.js
+++
@@ -1,9 +0,0 @@
-/*const myName = "Alex"; 
-myName = "Nagi"; // myName is const so cannot reassign to another value
-console.log(myName);
-*/
-
-// Fix the code
-let myName = "Alex"; 
-myName = "Nagi"; // myName is const so cannot reassign to another value
-console.log(myName);
