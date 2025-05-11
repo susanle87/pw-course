879decbe44fed95e9034e41fcda879608f6a122b
PW-Phuong <phuong2111@gmail.com>
2025-05-11 18:05:05 +0700

    feat: add tests/lesson-2/ex4.js

--- tests/lesson-2/ex4.js
+++ tests/lesson-2/ex4.js
@@ -1,2 +1,5 @@
 let height =170; // 
-let maxWeight= het 
+let maxWeight= (height-100);
+let minWeight=(maxWeight*8)/10
+let idealweight =(maxWeight)*9/10
+console.log ("Cân nặng lí tưởng: ", idealweight , ", Cân Nặng tối đa: ", maxWeight ," , Cân nặng tối thiểu :", minWeight);
